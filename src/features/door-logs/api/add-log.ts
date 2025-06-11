30a44002096ba8a8252941e8d273e4dfa1164e56
Dimpfe <dimasprasetyo7696@gmail.com>
2025-06-11 09:48:09 +0700

    feat<>: create doorlogs feature

---
+++ src/features/door-logs/api/add-log.ts
@@ -0,0 +1,22 @@
+
+
+type TCredentials = {
+   fingerprintId: number;
+   rfId: string;
+}
+
+export const addLog = async ({ fingerprintId, rfId }:TCredentials  ) => {
+   const response = await fetch(`/api/door-logs`, {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+         fingerprintId,
+         rfId,
+      }),
+   });
+   const data = await response.json();
+
+   return data;
+}
